c3651c28416d85c1f0b303f68b4eaeb50d493db5
Anshul <anshulsoni2003@gmail.com>
2023-12-24 21:25:34 +0530

    fix: key error

--- src/pages/Prioritizer.jsx
+++ src/pages/Prioritizer.jsx
@@ -7,7 +7,7 @@ import SearchIssue from "../SearchIssue";
 import axios from "axios";
 
 const supabaseUrl = 'https://dbsedophonqpzrnseplm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRic2Vkb3Bob25xcHpybnNlcGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk3MTA1NDUsImV4cCI6MjAxNTI4NjU0NX0.vMPEc1zF9PKvA5UCCMUutR__Z-cpfUY9pJZCvE';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRic2Vkb3Bob25xcHpybnNlcGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk3MTA1NDUsImV4cCI6MjAxNTI4NjU0NX0.vMPEc1zF9PKvA5UCCMUutR__Z-cpfUY9pKzUsYJZCvE';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function App() {
