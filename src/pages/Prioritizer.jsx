4f178a28482a3f83d3071f9037db68a33d7e4b8c
Anshul <anshulsoni2003@gmail.com>
2024-01-02 16:47:20 +0530

    feat: render link

--- src/pages/Prioritizer.jsx
+++ src/pages/Prioritizer.jsx
@@ -4,10 +4,9 @@ import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import Profile from '../components/profile'
 import SearchIssue from "../SearchIssue";
-import axios from "axios";
 
 const supabaseUrl = 'https://dbsedophonqpzrnseplm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRic2Vkb3Bob25xcHpybnNlcGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk3MTA1NDUsImV4cCI6MjAxNTI4NjU0NX0.vMPEc1zF9PKvA5UCCMUutR__Z-cpfUY9pJZCvE';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRic2Vkb3Bob25xcHpybnNlcGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk3MTA1NDUsImV4cCI6MjAxNTI4NjU0NX0.vMPEc1zF9PKvA5UCCMUutR__Z-cpfUY9pKzUsYJZCvE';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function App() {
@@ -75,7 +74,7 @@ export default function App() {
         const issuesData = await response.json();
         console.log("issues data:", issuesData)
 
-        const prioritiesResponse = await fetch('http://localhost:5000/predict', {
+        const prioritiesResponse = await fetch('https://priority-server.onrender.com', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
